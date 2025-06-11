d8eb77638f8fb0cebebc0b87cb161618c5f45e7d
amarkoulavkar@gmail.com <amarkoulavkar@gmail.com>
2025-06-11 08:01:02 -0700

    updating fixing build errors

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json()); 
 
 const allowedOrigins = [
-  'http://localhost:5173/', // your frontend domain
+  'https://unicorn-cafe-management.vercel.app/', // your frontend domain
 ];
 
 app.use(cors({
@@ -30,5 +30,5 @@ app.use('/api/branches', require('./routes/branch.routes'));
 const revenueRoutes = require('./routes/revenue.routes');
 app.use('/api/revenue', revenueRoutes);
 app.use('/api/stats', require('./routes/user.routes'));
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
